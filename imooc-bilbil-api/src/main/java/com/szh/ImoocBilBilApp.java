795d9c046fb67d3ad4831ed89726dcbae826c9a2
Marico <3032388097@qq.com>
2023-06-11 17:23:26 +0800

    初始化项目

---
+++ imooc-bilbil-api/src/main/java/com/szh/ImoocBilBilApp.java
@@ -0,0 +1,17 @@
+package com.szh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by szh on 2023-06-11
+ *
+ * @author szh
+ */
+
+@SpringBootApplication
+public class ImoocBilBilApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocBilBilApp.class, args);
+    }
+}
